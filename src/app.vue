4b36a84fe7015f6023759cbb9337f2c4662b1c93
Andrew <andrewsc32@protonmail.com>
2019-02-21 07:03:11 -0500

    Able to search for full words.

--- src/app.vue
+++ src/app.vue
@@ -20,6 +20,23 @@ let letterMatrix = matrix.map((line) => {
     })
 })
 
+let directions = [[1,0], [1,1], [0,1], [-1,1], [-1,0], [-1,-1], [0, -1], [1,-1]];
+
+
+function recursiveSearch(letterMatrix, coord, dir, searchWord) {
+    let [ix,iy] = coord
+    if (searchWord.length === 0) {
+        return true
+    } else if (ix < 0 || ix >= 40 || iy < 0 || iy >= 40) {
+        return false;
+    } else if (searchWord[0] === letterMatrix[ix][iy].value) {
+        let found = recursiveSearch(letterMatrix, [ix+dir[0], iy+dir[1]], dir, searchWord.substr(1,searchWord.length));
+        letterMatrix[ix][iy].highlighted = letterMatrix[ix][iy].highlighted || found;
+        return found;
+    } else {
+        return false;
+    }
+}
 
 export default {
     data: function() {
@@ -32,15 +49,24 @@ export default {
     watch: {
         searchTerm(newValue, oldValue) {
             console.log(newValue);
-            let searchLetter = newValue.toUpperCase()[0] || ' ';
+            let searchWord = newValue.toUpperCase() || ' ';
+
+            // reset
             for (let ix=0; ix < this.letterMatrix.length; ix++) {
                 let row = this.letterMatrix[ix]
                 for (let iy=0; iy < row.length; iy++) {
                     let letter = row[iy];
-                    console.log(letter.value, letter.value === searchLetter);
-                    
-                        letter.highlighted = letter.value === searchLetter;
-                    
+                    letter.highlighted = false;
+                }
+            }
+            // search
+            for (let ix=0; ix < this.letterMatrix.length; ix++) {
+                let row = this.letterMatrix[ix]
+                for (let iy=0; iy < row.length; iy++) {
+                    let letter = row[iy];
+                    for (let dir of directions) {
+                        recursiveSearch(this.letterMatrix, [ix,iy], dir, searchWord)
+                    }
                 }
             }
         }
